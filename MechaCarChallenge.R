bbe99a78c57e3b698ab3783ebec4690bac2db58e
iliarafa <83038866+iliarafa@users.noreply.github.com>
2021-09-23 14:05:09 -0400

    push del2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,8 @@
 library(dplyr)
 mechacar <- read.csv(file='MechaCar_mpg.csv')
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar))
+suspension <- read.csv("Suspension_Coil.csv")
+total_summary <- suspension %>% summarize(mean=mean(PSI),median=median(PSI),variance=var(PSI),sd=sd(PSI))
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+t.test(suspension$PSI, mu=1500)
+
